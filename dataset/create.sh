8f7f814d8f024edf9d4a70bf1e58f8cf54310f47
PeerDavid <david.peer.1991@gmail.com>
2017-04-10 18:33:02 +0200

    Renamings

---
+++ dataset/create.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 
+# Create a dataset from flick. 
+# Simply call ./dataset_create.sh car book face [...]
+# The classes will be numbered (car=0, book=1, face=2, ...)
+# and downloaded into the downloads folder. This folder can
+# later be used to train the cnn
+#
+
+# Configuration
+CLASSES=("$@")
+DOWNLOAD_PATH="data/train"
+NUM_IMAGES=1000
+
+# Prepare downloads folder
+if [ ! -d "$DOWNLOAD_PATH" ]; then
+    rm -rf $DOWNLOAD_PATH
+    mkdir $DOWNLOAD_PATH
+
+    # First of all download all images from flickr
+    classId=0
+    for className in "${CLASSES[@]}"
+    do
+        python dataset_download.py "$DOWNLOAD_PATH/$classId" $className $NUM_IMAGES
+        let "classId=classId+1"
+    done
+
+    # Now preprocess images to 366x366 pixels because we crop 
+    # it randomly to 299x299 for the inception v3 model to 
+    # increase the training set size (data augmentation)
+    python3 dataset_preprocess.py "$DOWNLOAD_PATH/"
+fi
